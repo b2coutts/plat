a5b8bcc51ea6fa7994b44649353bc9620966f7cd
Bryan Coutts <b2coutts@uwaterloo.ca>
2016-08-24 14:49:39 -0400

    Made different jump heights a bit nicer
    still not fully satisfied, will play around a bit when I have actual levels

--- params.py
+++ params.py
@@ -13,13 +13,15 @@ FRAME_LENGTH            = 0.0167
 #FRAME_LENGTH           = 0.167
 RUN_SPEED               = 3.5
 AIR_SPEED               = 3.5
-JUMP_ACCEL              = 1.4
-JUMP_ACCEL_LEN          = 6
+JUMP_ACCEL              = 1.7
+JUMP_ACCEL_LEN          = 5
 
 AIR_ACCEL               = 0.3
 FRIC_DECEL              = 0.1
 FRIC_DECEL_SLOW         = 0.00
 FRIC_DECEL_SLOW_THRES   = 0.75
 GRAV_ACCEL              = 0.5
+GRAV_ACCEL_RAT_MIN      = 0.5
+GRAV_ACCEL_RAT_COEF     = (GRAV_ACCEL_RAT_MIN-1) / (JUMP_ACCEL*JUMP_ACCEL_LEN)
 
 BOOST_SPEED             = 8.0
